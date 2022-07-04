c2695577a215ddace799cda7d52c5c2da0b2a4f1
MaximArzhanov <arzhanov93@yandex.ru>
2022-07-04 21:04:53 +0600

    Добавлено модальное окно

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,12 +3,31 @@ import './Button.css';
 
 function Button() {
 
+  const [popupIsOpen, setPopupIsOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setPopupIsOpen(!popupIsOpen);
+  }
+
+  const classList = popupIsOpen ? 'popup popup__open' : 'popup';
+
   return (
-    <div className="button">
-      <span className="toc__hamburger-box">
-        <span className="toc__hamburger-inner"></span>
-      </span>
+    <div>
+
+      <button className="toc__hamburger js-hamburger" onClick={handleClick}>
+        <span className="toc__hamburger-box">
+          <span className="toc__hamburger-inner"></span>
+        </span>
+        <div className="toc__hamburger-label">Меню</div>
+      </button>
+
+      <div className={classList}>
+        Контент
+      </div>
+
+
     </div>
+
   );
 }
 
