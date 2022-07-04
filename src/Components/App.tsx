97aee3c591c2448ffc902ded2121e9f0d5df5369
MaximArzhanov <arzhanov93@yandex.ru>
2022-07-04 17:24:49 +0600

    feat: Создан тестовый проект

---
+++ src/Components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from './Button/Button';
+
+function App() {
+
+  return (
+    <div className="app">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+
+  return (
+    <div className="button">
+      <span className="toc__hamburger-box">
+        <span className="toc__hamburger-inner"></span>
+      </span>
+    </div>
+  );
+}
+
+export default Button;
